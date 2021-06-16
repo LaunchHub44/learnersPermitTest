596f9943598143365aa80d1e72678517ef0d1794
LaunchHub44 <jakechung2222@gmail.com>
2021-06-15 20:37:23 -0400

    Basic structure for learner's permit test.

--- learn.py
+++ learn.py
@@ -13,3 +13,22 @@ NOTE:  Debian useful tip:
 
 """
 
+import sqlite3
+
+questions = {}    # ( key=image-name,  value=rowid-of-answer )
+answers = {}      # ( key=rowid,   value=( short, long) )
+
+db = sqlite3.connect("questions.db")
+cur = db.cursor()
+
+cur.execute("select image, answer from question")
+for row in cur.fetchall():
+  questions[ row[0]] = row[1]
+
+cur.execute("select rowid, short_desc, long_desc from choice")
+for row in cur.fetchall():
+  answers[row[0]] = (row[1], row[2])
+
+## DEBUG TEST
+print( questions["No-Left.png"], answers[6] )
+print( answers[6][0], answers[6][1])
